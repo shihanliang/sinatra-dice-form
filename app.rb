5b11e2214e35b64c9fb6c53f277cba25e237ec8a
shihanliang <sliang1@ChicagoBooth.edu>
2025-04-21 04:43:14 +0000

    code

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb :form
+end
+
+get("/process_roll") do
+  @dice = params[:dice].to_i
+  @sides = params[:sides].to_i
+  @rolls = Array.new(@dice) { rand(1..@sides) }
+  erb :result
 end
